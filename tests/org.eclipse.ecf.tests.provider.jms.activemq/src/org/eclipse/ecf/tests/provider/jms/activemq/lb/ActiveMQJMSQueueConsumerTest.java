886674050a8b872631321b388c4e8f290104bc84
slewis@composent.com <slewis@composent.com>
2010-10-31 06:33:17 -0700

    Made activemq queue consumer and queue producer tests abstract until completed.

--- tests/org.eclipse.ecf.tests.provider.jms.activemq/src/org/eclipse/ecf/tests/provider/jms/activemq/lb/ActiveMQJMSQueueConsumerTest.java
+++ tests/org.eclipse.ecf.tests.provider.jms.activemq/src/org/eclipse/ecf/tests/provider/jms/activemq/lb/ActiveMQJMSQueueConsumerTest.java
@@ -5,7 +5,7 @@ import org.eclipse.ecf.core.ContainerFactory;
 import org.eclipse.ecf.core.IContainer;
 import org.eclipse.ecf.tests.ContainerAbstractTestCase;
 
-public class ActiveMQJMSQueueConsumerTest extends ContainerAbstractTestCase {
+public abstract class ActiveMQJMSQueueConsumerTest extends ContainerAbstractTestCase {
 
 	public void testConnectConsumer() throws Exception {
 		IContainer container = ContainerFactory.getDefault().createContainer(ActiveMQLB.QUEUE_CONSUMER_CONTAINER_NAME, new Object[] { ActiveMQLB.QUEUE_NAME });
--- tests/org.eclipse.ecf.tests.provider.jms.activemq/src/org/eclipse/ecf/tests/provider/jms/activemq/lb/ActiveMQJMSQueueProducerTest.java
+++ tests/org.eclipse.ecf.tests.provider.jms.activemq/src/org/eclipse/ecf/tests/provider/jms/activemq/lb/ActiveMQJMSQueueProducerTest.java
@@ -5,7 +5,7 @@ import org.eclipse.ecf.core.ContainerFactory;
 import org.eclipse.ecf.core.IContainer;
 import org.eclipse.ecf.tests.ContainerAbstractTestCase;
 
-public class ActiveMQJMSQueueProducerTest extends ContainerAbstractTestCase {
+public abstract class ActiveMQJMSQueueProducerTest extends ContainerAbstractTestCase {
 
 	public void testConnectConsumer() throws Exception {
 		IContainer container = ContainerFactory.getDefault().createContainer(ActiveMQLB.QUEUE_PRODUCER_CONTAINER_NAME, new Object[] { ActiveMQLB.TOPIC_NAME, ActiveMQLB.QUEUE_NAME });
